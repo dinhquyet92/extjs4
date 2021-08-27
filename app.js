1f640332d1ed8552516b16145851fd2e31fa856e
DinhQuyet <dinhquyet.92@gmail.com>
2021-08-27 17:41:19 +0700

    MVC: Defining a View

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 Ext.require('Ext.container.Viewport');
+// Ext.application({
+//     name: 'HelloExt',
+//     launch: function() {
+//         Ext.create('Ext.container.Viewport', {
+//             layout: 'fit',
+//             items: [
+//                 {
+//                     title: 'Hello ExtJs',
+//                     html : '<h1 class="text-center">Hello! Welcome to Ext JS.</h1>'
+//                 }
+//             ]
+//         });
+//     }
+// });
 Ext.application({
-    name: 'HelloExt',
+    name: 'AM',
+
+    appFolder: 'app',
+
     launch: function() {
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: [
                 {
-                    title: 'Hello ExtJs',
-                    html : '<h1 class="text-center">Hello! Welcome to Ext JS.</h1>'
+                    xtype: 'userlist',
+                    title: 'Users',
+                    html : '<h1>List of users will go here</h1>'
                 }
             ]
         });
-    }
+        console.log('Initialized Application! ');
+    },
+    controllers: [
+        'Users'
+    ]
 });
